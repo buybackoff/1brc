3644b251cda38abd620bda644efda12951020042
Victor Baybekov <vbaybekov@gmail.com>
2024-01-06 19:22:54 +0100

    Update Readme, add basic scripts

---
+++ build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+dotnet publish -r linux-x64 -c Release
---
+++ run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+time dotnet run --project 1brc/1brc.csproj -c Release --no-build  -- ~/dev/1brc/java/1brc/measurements.txt
