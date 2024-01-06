e4396cc61c9572af849e17cc6625e47bce6b2960
Victor Baybekov <vbaybekov@gmail.com>
2024-01-06 15:37:50 +0100

    Update Readme

--- 1brc/Program.cs
+++ 1brc/Program.cs
@@ -7,11 +7,12 @@ class Program
     static void Main(string[] args)
     {
         var sw = Stopwatch.StartNew();
-        var path = args.Length > 0 ? args[0] : "D:/tmp/1brc_1B.txt";
+        var path = args.Length > 0 ? args[0] : "D:/tmp/1brc_temp/1brc_1B.txt";
         using var app = new App(path);
         // Console.WriteLine($"Chunk count: {app.SplitIntoMemoryChunks().Count}");
         app.PrintResult();
         sw.Stop();
         Console.WriteLine($"Processed in {sw.Elapsed}");
+        Environment.Exit(0);
     }
 }
