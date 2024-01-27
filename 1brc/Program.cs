f9dcdcd0063a10052f67fdf53edc19a55b49512b
Victor Baybekov <vbaybekov@gmail.com>
2024-01-27 10:36:53 +0100

    Do not mess with process priorities

--- 1brc/Program.cs
+++ 1brc/Program.cs
@@ -11,14 +11,12 @@ internal class Program
     private static void Main(string[] args)
     {
         var sw = Stopwatch.StartNew();
-        Process.GetCurrentProcess().PriorityClass = ProcessPriorityClass.High;
         Console.OutputEncoding = Encoding.UTF8;
         var path = args.Length > 0 ? args[0] : "D:/tmp/measurements_1B_10K.txt";
         using (var app = new App(path))
         {
             app.PrintResult();
         }
-
         sw.Stop();
         Console.WriteLine($"Finished in: {sw.ElapsedMilliseconds:N0} ms");
     }
